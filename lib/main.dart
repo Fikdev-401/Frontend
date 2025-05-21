2c02d47b85089a096d69109aad802bba8b8b99f6
fikdev-401 <fikdev.my.id@gmail.com>
2025-05-22 00:15:30 +0700

    add: device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:device_preview/device_preview.dart'; // Tambahkan ini
+import 'package:flutter_localizations/flutter_localizations.dart'; // Untuk mendukung device_preview
+
+// Import bloc dan lain-lain
 import 'package:flutter_frontend/bloc/addGoals/add_goal_bloc.dart';
 import 'package:flutter_frontend/bloc/addJournals/bloc/add_journal_bloc.dart';
 import 'package:flutter_frontend/bloc/goals/goals_bloc.dart';
@@ -17,56 +22,64 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-
-// import 'core/core.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('id_ID', null); // Inisialisasi Bahasa Indonesia
+  await initializeDateFormatting('id_ID', null);
   Intl.defaultLocale = 'id_ID';
-  runApp(const MyApp());
+
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    
     final remoteDatasource = RemoteDatasource();
     return MultiBlocProvider(
-        providers: [
-          BlocProvider(create: (_) => LoginBloc()),
-          BlocProvider(create: (_) => RegisterBloc()),
-          BlocProvider(
-              create: (_) => GoalsBloc(remoteDatasource: remoteDatasource)),
-          BlocProvider(
-              create: (_) => JournalsBloc(remoteDatasource: remoteDatasource)),
-          BlocProvider(
-              create: (_) =>
-                  CategoryGoalBloc(remoteDatasource: remoteDatasource)),
-          BlocProvider(create: (_) => AddGoalBloc()),
-          BlocProvider(create: (_) => AddJournalBloc(),
+      providers: [
+        BlocProvider(create: (_) => LoginBloc()),
+        BlocProvider(create: (_) => RegisterBloc()),
+        BlocProvider(create: (_) => GoalsBloc(remoteDatasource: remoteDatasource)),
+        BlocProvider(create: (_) => JournalsBloc(remoteDatasource: remoteDatasource)),
+        BlocProvider(create: (_) => CategoryGoalBloc(remoteDatasource: remoteDatasource)),
+        BlocProvider(create: (_) => AddGoalBloc()),
+        BlocProvider(
+          create: (_) => AddJournalBloc(),
           child: AddJournalPage(),
-          ),
-          BlocProvider(
-              create: (_) =>
-                  CategoryJournalBloc(remoteDatasource: remoteDatasource)),
-          BlocProvider(create: (_) => EditGoalBloc()),
-          BlocProvider(create: (_) => EditJournalBloc()),
-          BlocProvider(create: (_) => DeleteJournalBloc(remoteDatasource: remoteDatasource)),
-          BlocProvider(create: (_) => DeleteGoalBloc(remoteDatasource: remoteDatasource)),
-          BlocProvider(create: (_) => UserBloc()),
-          
+        ),
+        BlocProvider(create: (_) => CategoryJournalBloc(remoteDatasource: remoteDatasource)),
+        BlocProvider(create: (_) => EditGoalBloc()),
+        BlocProvider(create: (_) => EditJournalBloc()),
+        BlocProvider(create: (_) => DeleteJournalBloc(remoteDatasource: remoteDatasource)),
+        BlocProvider(create: (_) => DeleteGoalBloc(remoteDatasource: remoteDatasource)),
+        BlocProvider(create: (_) => UserBloc()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        useInheritedMediaQuery: true, // wajib untuk device_preview
+        locale: DevicePreview.locale(context), // dari device_preview
+        builder: DevicePreview.appBuilder, // dari device_preview
+        home: const SplashPage(),
+        routes: routes,
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(),
+        ),
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
         ],
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: const SplashPage(),
-          routes: routes,
-          theme: ThemeData(
-            textTheme: GoogleFonts.poppinsTextTheme(),
-          ),
-        ));
+        supportedLocales: const [
+          Locale('en', 'US'),
+          Locale('id', 'ID'),
+        ],
+      ),
+    );
   }
 }
